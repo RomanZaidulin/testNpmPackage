66d14ebd150411ba4209ebaa1e9e469e3fc06f26
Роман Зайдулин <roman.zaidulin2015@gmail.com>
2025-05-14 10:10:34 +0300

    save

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+//import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" with { type: "json" };
 
 export default [
@@ -25,9 +25,9 @@ export default [
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
   },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-  },
+  // {
+  //   input: "dist/esm/types/index.d.ts",
+  //   output: [{ file: "dist/index.d.ts", format: "esm" }],
+  //   plugins: [dts()],
+  // },
 ];
